81129c9c65a09f2fb5552db76043c23fcbd79a03
Arshaan <arshaan.bhimani@uwaterloo.ca>
2023-10-23 22:44:44 -0400

    Removed Next.js default styling (e.g. backgroundColor) and added header

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,10 @@ import Table from "./components/Table";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-6">
+    <main className="flex min-h-screen flex-col items-center p-6">
+      <div className="bg-white text-black p-4 text-center">
+        <h1 className="text-3xl font-bold">Telepathy Message Explorer</h1>
+      </div>
       <Table />
     </main>
   );
