10438dbc41b8df6d2abf802ece9e2c66341d369f
Arshaan <arshaan.bhimani@uwaterloo.ca>
2023-10-30 00:27:25 -0400

    Updated messages.tsx to grab latest block and grab 10,000 blocks at a time

--- app/utils/messages.tsx
+++ app/utils/messages.tsx
@@ -1,6 +1,11 @@
+import { Core } from "@quicknode/sdk";
+
 import { MessageType } from "./Types";
 
-export async function getLogs() {
+export async function getLogs(fromBlock: number, toBlock: number) {
+  const fromBlockHex = `0x${fromBlock.toString(16)}`;
+  const toBlockHex = `0x${toBlock.toString(16)}`;
+
   const result = await fetch("https://docs-demo.quiknode.pro/", {
     method: "POST",
     headers: {
@@ -10,8 +15,8 @@ export async function getLogs() {
       method: "eth_getLogs",
       params: [
         {
-          fromBlock: "0x1191704",
-          toBlock: "0x1193E13",
+          fromBlock: fromBlockHex,
+          toBlock: toBlockHex,
           address: "0x41EA857C32c8Cb42EEFa00AF67862eCFf4eB795a",
           topics: [
             "0xe5944a34d67c652e0ebf2304b48432aae0b55e40f79ba8a21a4d7054c169ffac"
@@ -22,14 +27,20 @@ export async function getLogs() {
       jsonrpc: "2.0"
     })
   });
-
   return result.json();
 }
 
 export async function getMessages(
   setMessages: (messages: MessageType[]) => void
 ) {
-  const { result: messages } = await getLogs();
+  const core = new Core({
+    endpointUrl: "https://docs-demo.quiknode.pro/"
+  });
+  const currentBlock = Number(await core.client.getBlockNumber());
+  const { result: messages } = await getLogs(
+    currentBlock - 10000,
+    currentBlock
+  );
   if (messages) {
     const reverseMessages = [];
     for (let messageIdx = messages.length - 1; messageIdx >= 0; messageIdx--) {
