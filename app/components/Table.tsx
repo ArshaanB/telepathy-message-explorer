2e4ad7243f7b58927b3119b8c3eb95f3a018bbd7
Arshaan <arshaan.bhimani@uwaterloo.ca>
2023-10-31 03:47:49 -0400

    For Table.tsx added comments throughout the file, useCallback optimization, state initialized to undefined, added tx hash Links

--- app/components/Table.tsx
+++ app/components/Table.tsx
@@ -1,4 +1,5 @@
-import { useState, useEffect } from "react";
+import Link from "next/link";
+import { useState, useEffect, useCallback } from "react";
 import { useInfiniteQuery } from "@tanstack/react-query";
 
 import { Input } from "@/components/ui/input";
@@ -15,30 +16,67 @@ import {
 
 import { getLogs, getCurrentBlock, formatMessages } from "../utils/messages";
 
-export default function Component() {
-  const BLOCK_RANGE = 10000;
-  const PAGE_SIZE = 10;
+const BLOCK_RANGE = 10000;
+const PAGE_SIZE = 10;
 
+/**
+ * This component displays a table of messages. It allows users to browse and
+ * filter messages by block numbers.
+ * It uses the react-query library to fetch data in an "infinite" manner.
+ */
+export default function Component() {
+  /**
+   * The state variables for the component:
+   * - displayPage: The current page being displayed ("current" or "filtered").
+   * - fromBlockFilter: The starting block number for the filter.
+   * - toBlockFilter: The ending block number for the filter.
+   * - buffer: The buffer that holds the messages data.
+   * - pageIndex: The current page index.
+   * - currentBlock: The current block number.
+   */
   const [displayPage, setDisplayPage] = useState("current");
-  const [fromBlockFilter, setFromBlockFilter] = useState(null);
-  const [toBlockFilter, setToBlockFilter] = useState(null);
+  const [fromBlockFilter, setFromBlockFilter] = useState(undefined);
+  const [toBlockFilter, setToBlockFilter] = useState(undefined);
   const [buffer, setBuffer] = useState([]);
   const [pageIndex, setPageIndex] = useState(0);
   const [currentBlock, setCurrentBlock] = useState(null);
 
+  /**
+   * Fetch the current block number when the component mounts.
+   */
   useEffect(() => {
-    getCurrentBlock().then((currentBlock) => setCurrentBlock(currentBlock));
+    const fetchCurrentBlock = async () => {
+      try {
+        const currentBlock = await getCurrentBlock();
+        setCurrentBlock(currentBlock);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchCurrentBlock();
   }, []);
 
-  // For a given pageParam (and it's unique 10,000 block range), returns all the
-  //  relevant logs within that block range.
-  async function fetchMessages({ pageParam = 0 }) {
-    const fromBlock = currentBlock - (pageParam + 1) * BLOCK_RANGE;
-    const toBlock = currentBlock - pageParam * BLOCK_RANGE;
-    const logs = await getLogs(fromBlock, toBlock);
-    return logs.result;
-  }
+  /**
+   * Callback to fetch messages data. It calculates the block range based on the
+   * current block number and the page parameter.
+   *
+   * @returns {Promise<Array>} The fetched messages data.
+   */
+  const fetchMessages = useCallback(
+    async ({ pageParam = 0 }) => {
+      const fromBlock = currentBlock - (pageParam + 1) * BLOCK_RANGE;
+      const toBlock = currentBlock - pageParam * BLOCK_RANGE;
+      const logs = await getLogs(fromBlock, toBlock);
+      return logs.result;
+    },
+    [currentBlock]
+  );
 
+  /**
+   * An infinite query to fetch messages data.
+   * It only runs when the current block number is not null.
+   */
   const { data, fetchNextPage, hasNextPage, isFetchingNextPage } =
     useInfiniteQuery(["messages"], fetchMessages, {
       // pages holds how many calls we have made already, and each page holds
@@ -50,6 +88,9 @@ export default function Component() {
       enabled: currentBlock !== null
     });
 
+  /**
+   * Formats and adds the new data to the buffer when the data changes.
+   */
   useEffect(() => {
     if (data) {
       const newData = data.pages[data.pages.length - 1];
@@ -61,9 +102,13 @@ export default function Component() {
     }
   }, [data]);
 
+  /**
+   * Function to go to the next page. It fetches more messages data until there
+   * are enough messages for the next page.
+   */
   const nextPage = () => {
     const fetchUntilEnoughMessages = async (currentBuffer) => {
-      if (currentBuffer.length < (pageIndex + 2) * 10) {
+      if (currentBuffer.length < (pageIndex + 2) * PAGE_SIZE) {
         const newBuffer = await fetchNextPage();
         const totalLengthOfNewBuffer = newBuffer.data.pages.reduce(
           (sum, page) => {
@@ -80,20 +125,34 @@ export default function Component() {
     });
   };
 
+  /**
+   * Function to go to the previous page.
+   */
   const previousPage = () => {
     setPageIndex((oldIndex) => Math.max(oldIndex - 1, 0));
   };
 
+  /**
+   * The current page data.
+   */
   const currentPage = buffer?.slice(
     pageIndex * PAGE_SIZE,
     (pageIndex + 1) * PAGE_SIZE
   );
 
+  /**
+   * The filtered page data.
+   */
   const filteredPage = buffer?.filter(
     (item) =>
       item.blockNumber >= fromBlockFilter && item.blockNumber <= toBlockFilter
   );
 
+  /**
+   * Toggles the expanded state of a message at a specific index.
+   *
+   * @param {number} index The index of the message
+   */
   function updateIsExpandedForIndex(index) {
     const paginatedIndex = pageIndex * PAGE_SIZE + index;
     const isExpandedIntermediate = [...buffer];
@@ -190,7 +249,13 @@ export default function Component() {
                         </button>
                       </TableCell>
                       <TableCell className="break-words overflow-wrap w-96">
-                        {item.transactionHash}
+                        <Link
+                          href={`https://etherscan.io/tx/${item.transactionHash}`}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                        >
+                          {item.transactionHash}
+                        </Link>
                       </TableCell>
                     </TableRow>
                   ))}
